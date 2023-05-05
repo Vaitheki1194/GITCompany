e87beb010e74e594e41364dc4839648025c4b1f9
Vaitheki1194 <vaitheki.1194@gmail.com>
2023-05-05 07:14:52 +0530

    Task done

---
+++ src/test/java/org/cts/Employee.java
@@ -0,0 +1,11 @@
+package org.cts;
+
+public class Employee {
+public void add() {
+	System.out.println("GITProject1");
+}
+public static void main(String[] args) {
+	Employee e = new Employee();
+	e.add();
+}
+}
